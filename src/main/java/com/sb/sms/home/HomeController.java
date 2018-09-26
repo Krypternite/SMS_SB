a2124c2a9707145f64a722403027b0dc42496d09
Samad Ali Sadiq <samad@theteamie.com>
2018-09-27 04:55:12 +0530

    First Commit

---
+++ src/main/java/com/sb/sms/home/HomeController.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sb.sms.home;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author samad
+ */
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+   public String hello() {
+        return "Hello World!t";
+    }
+}
